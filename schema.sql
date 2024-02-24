32f11739a87b0133cac4e2aab3cbe58095cb4b63
John (Chun Han) Lee <clee93@nait.ca>
2024-02-23 22:02:21 -0700

    Current progress for website project.

---
+++ schema.sql
@@ -0,0 +1,22 @@
+
+
+DROP TABLE IF EXISTS FoodInfo;
+DROP TABLE IF EXISTS Food_Category;
+
+CREATE TABLE FoodInfo (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category_id INT,
+    [name] VARCHAR(50),
+    nickname VARCHAR(50),
+    taste VARCHAR(50),
+    efficacy TEXT,
+    suitable_for TEXT,
+    not_suitable_for TEXT,
+    note TEXT,
+    FOREIGN KEY (category_id) REFERENCES Food_Category(id)
+);
+
+CREATE TABLE Food_Category (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50)
+);
